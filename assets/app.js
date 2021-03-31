74c9470b362a4e4456a28e9edc6ce68d4c062a95
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-03-31 15:04:00 -0400

    start game logic

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,15 @@
+//loads the page and also connects the html classes to the javascript
 document.addEventListener('DOMContentLoaded', () => {
 	const bird = document.querySelector('.bird');
+	const gameDisplay = document.querySelector('.game-container');
+	const ground = document.querySelector('.ground');
+
+	let birdLeft = 220;
+	let birdBottom = 100;
+
+	function startGame() {
+		bird.style.bottom = birdBottom + 'px';
+	}
+
+	startGame();
 });
