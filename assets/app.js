9ab18f8819b47a8b9b235a2a116addb14d5790aa
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-04-01 17:11:57 -0400

    game works

--- assets/app.js
+++ assets/app.js
@@ -2,11 +2,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const bird = document.querySelector('.bird');
 	const gameDisplay = document.querySelector('.game-container');
-	const ground = document.querySelector('.ground');
-
+	const ground = document.querySelector('.ground-moving');
 	let birdLeft = 220;
 	let birdBottom = 100;
-	let gravity = 2;
+	let gravity = 3;
 	let isGameOver = false;
 	let gap = 430;
 
@@ -55,12 +54,18 @@ document.addEventListener('DOMContentLoaded', () => {
 		function moveObstacle() {
 			obstacleLeft -= 2;
 			obstacle.style.left = obstacleLeft + 'px';
+			topObstacle.style.left = obstacleLeft + 'px';
+
 			if (obstacleLeft === -60) {
 				clearInterval(timerId);
 				gameDisplay.removeChild(obstacle);
+				gameDisplay.removeChild(topObstacle);
 			}
 			if (
-				(obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 && birdBottom < obstacleBottom + 153) ||
+				(obstacleLeft > 200 &&
+					obstacleLeft < 280 &&
+					birdLeft === 220 &&
+					(birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap - 200)) ||
 				birdBottom === 0
 			) {
 				gameOver();
@@ -79,5 +84,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		console.log('game over');
 		isGameOver = true;
 		document.removeEventListener('keyup', control);
+		ground.classList.add('ground');
+		ground.classList.remove('ground-moving');
 	}
 });
