3ff91d54afbf72f3517660103fa4a99e9af4f179
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-04-01 14:54:43 -0400

    styled obstacles a bit

--- assets/app.js
+++ assets/app.js
@@ -34,9 +34,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	//logic for obstacles
 	function generateObstacle() {
+		let obstacleLeft = 500;
+		let obstacleBottom = 150;
 		const obstacle = document.createElement('div');
 		obstacle.classList.add('obstacle');
 		gameDisplay.appendChild(obstacle);
+		obstacle.style.left = obstacleLeft + 'px';
+		obstacle.style.bottom = obstacleBottom + 'px';
 	}
 
 	generateObstacle();
